0baf6c194a3192f2d17e4e09fd15d8ca41bca21b
Denny Setyawan Huda <dennshgit@gmail.com>
2022-11-29 13:46:42 +0700

    feat: create database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
@@ -8,6 +9,15 @@ const port = 3000;
 app.set("view-engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB");
+
+const articleSchema = {
+  title: String,
+  content: String,
+};
+
+const Article = mongoose.model("Article", articleSchema);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
